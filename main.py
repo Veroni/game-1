4907f10b678d4df8923239689fbd176f4e61aac1
Veroni <veronika@zisel.ca>
2021-05-15 08:21:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ tk.title("Game#1")
 tk.resizable(0, 0)
 canvas = Canvas(tk, width=400, height=400, bd=0, highlightthickness=0)
 canvas.pack()
+
+board = []
+
+def print_board:
+  for x in range(0,5):
+    board.append(["O"] * 5)
+    print(board)
